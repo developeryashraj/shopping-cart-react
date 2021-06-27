c6d70adc18bc3f97345b4f800f21f3d996325eb4
Yashrajsinh Jadeja <developer.yashraj@gmail.com>
2021-06-27 18:21:52 +0000

    Removed unwanted code

--- src/utils/cart.spec.js
+++
@@ -1,7 +0,0 @@
-import prepareCart from "./cart";
-
-describe("prepareCart scenarios", function () {
-  it("Should get cartProducts as blank array", function () {
-    expect(prepareCart()).toBe({});
-  });
-});
